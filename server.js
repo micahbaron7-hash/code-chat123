0bbf8f8b65875b34b5c21a63b32d17f4cb99e2a4
micahbaron7-hash <micahbaron7@gmail.com>
2026-09-11 16:55:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const rooms = new Map();
 
